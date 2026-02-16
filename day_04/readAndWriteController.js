e3d435015e3125a5d01fb168bb823e301fdf8961
Ahmad Hammad Khan <ahmadhammadkhan21112005@gmail.com>
2026-02-16 12:57:50 +0530

    readAndWriteController

--- day_04/readAndWriteController.js
+++ day_04/readAndWriteController.js
@@ -1,3 +1,4 @@
+import fs from "fs/promises";
 import {readFile} from "./readAndWriteFile.js";
 
 const readFileData = async(path) => {
@@ -8,4 +9,23 @@ const readFileData = async(path) => {
         console.log("Some error occured");
     }
 }
+
+const writeFileData = async (path , data) => {
+    try {
+        const fileData = await readFile(path);
+        fileData.push(data);
+        await fs.writeFile(path , JSON.stringify(fileData , null , 2) , "utf-8");
+        console.log("Student added successfully");
+    } catch (error) {
+        console.log("Some error occured during writing");
+    }
+}
 readFileData("./students.json");
+writeFileData("./students.json" , {
+    id: 13,
+    first_name: "Inshu",
+    last_name: "Pervez",
+    email: "inshu@utexas.edu",
+    gender: "female",
+    ip_address: "1.21.96.120"
+});
--- day_04/readAndWriteFile.js
+++ day_04/readAndWriteFile.js
@@ -7,6 +7,37 @@ export const readFile = async (path) => {
         return "Error occured during reading";
     }
 }
+
+export const writeFile = async (path , data) => {
+    try {
+        const jsonData = JSON.stringify(data , null , 2);
+        await fs.writeFile(path , jsonData , "utf-8");
+        console.log("data has been written successfully");
+    } catch (error) {
+        console.log("Unabe to write data to file using writeFile fs");
+    }
+}
+const students = [
+    {
+        "id": 11,
+        "first_name": "Aditya",
+        "last_name": "Tayal",
+        "email": "aditayal@utexas.edu",
+        "gender": "Tran",
+        "ip_address": "1.21.96.120"
+    },
+    {
+        "id": 12,
+        "first_name": "Ahmad",
+        "last_name": "Hammad",
+        "email": "hamad2@utexas.edu",
+        "gender": "male",
+        "ip_address": "1.21.96.120"
+    }    
+]
+writeFile("./students.json" , students)
+
 readFile("./students.json")
     .then((data) => console.log(data))
     .catch((error) => console.log("Some error occured"));
+
