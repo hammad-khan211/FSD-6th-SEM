1f879efcf75f14cfadc1a639ee245d7c3ef3fe45
Ahmad Hammad Khan <ahmadhammadkhan21112005@gmail.com>
2026-02-23 12:34:55 +0530

    Login created successful

---
+++ day_06/controller/login.js
@@ -0,0 +1,24 @@
+import {readFile} from "../utils/readFile.js";
+const FILE = "../user.json";
+const userLogin = async(userDetail) => {
+    const { email , password } = userDetail;
+    const users = await readFile(FILE);
+    if(users.length === 0){
+        console.log("User does not exist");
+        return;
+    }
+
+    const user = users.find((u) => u.email===email);
+    
+    if(!user){
+        console.log("user does not exist");
+        return;
+    }
+    (user.password === password ? console.log("Login Successful") : console.log("Incorrect Password"));
+
+}
+
+userLogin({
+  email: "aplanque9@admhin.ch",
+  password: "lG4)&#ILPC0Goo"
+});
--- day_06/utils/readFile.js
+++ day_06/utils/readFile.js
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 
-const readFile = async(path) => {
+export const readFile = async(path) => {
     try {
         const data = await fs.readFile(path,"utf-8");
         return JSON.parse(data);
