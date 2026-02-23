e700c2b08bbdab9ce3aa73dbacd782b5ed7f1dd8
Ahmad Hammad Khan <ahmadhammadkhan21112005@gmail.com>
2026-02-23 12:09:47 +0530

    login.js created

--- day_06/utils/readFile.js
+++ day_06/utils/readFile.js
@@ -3,10 +3,9 @@ import fs from "fs/promises";
 const readFile = async(path) => {
     try {
         const data = await fs.readFile(path,"utf-8");
-        console.log(data);
+        return JSON.parse(data);
     } catch (error) {
         console.log("Some error occured while reading the data");
     }
 }
 
-readFile("../user.json");
