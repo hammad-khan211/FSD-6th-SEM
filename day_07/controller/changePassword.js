868bf39f50435af5dbca829fb188edbe0590cf91
Ahmad Hammad Khan <ahmadhammadkhan21112005@gmail.com>
2026-02-25 14:45:42 +0530

    UpdatePassword.js created successfully

--- day_07/controller/changePassword.js
+++ day_07/controller/changePassword.js
@@ -1,2 +1,37 @@
 import { readFile } from "../../day_06/utils/readFile.js";
 import { writeFile } from "../../day_06/utils/writeFile.js";
+
+const FILE = "../../day_06/user.json";
+
+const updateUsers = async (users , userDetails) => {
+    const updatedData = users.map((u) => u.email === userDetails.email ? {...u, ...userDetails} : u);
+    const response = await writeFile(FILE , updatedData);
+    return response;
+}
+
+const changePassword = async (userDetails) => {
+    const { email , password } = userDetails;
+    if(!email || !password)
+    {
+        return { message : "All fields required " , status:401};
+    }
+    const users = await readFile(FILE);
+    if(users.length === 0)
+    {
+        return {message  : "User does not exist" , status :402};
+    }
+    const existingUser = users.find((u) => u.email === email);
+    if(!existingUser)
+    {
+        return {message  : "User does not exist" , status :402};
+    }
+    const response = await updateUsers(users , userDetails);
+    return response.status === 201 ?
+     {message : "Password updated successfully" , status:200} :
+     {message : "Unable to update password" , status : 500}
+}
+
+changePassword({
+    "email" : "ahmadhammadkhan21112005@gmail.com",
+    "password" : "hammad@new"
+}).then((response) => console.log(response));
