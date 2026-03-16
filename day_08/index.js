e480bc0a51dda3cbf1f5f974cc04a42acca24395
Ahmad Hammad Khan <ahmadhammadkhan21112005@gmail.com>
2026-03-16 13:04:50 +0530

    index.js completed

--- day_08/index.js
+++ day_08/index.js
@@ -1,21 +1,22 @@
 import http from "http";
+import { userLogin } from "../day_06/controller/login";
 
 const PORT = 8800;
 
 const server = http.createServer((req, res) => {
-    switch(req.url){
-        case "/" :
-            res.end("Home Page");
-            break;
-        case "/login" :
-            res.end("Login Page");
-            break;
-        case "/register" :
-            res.end("Register Page");
-            break;
-        default : 
-            res.end("Incorrect Path");
-    }
+    if(req.url === "/favicon.ico") return;
+    if(req.url === "/login" && req.method === "POST"){
+        let body = "";
+        req.on("data" , (dataChunk) => {
+            body += dataChunk.toString();
+        })
+        req.on("end" , async() => {
+            const userData = JSON.parse(body);
+            const response = await userLogin(userData , "./user.json");
+            res.writeHead(200 , {"content-type":"application/json"});
+            res.end(JSON.stringify(response));
+        })
+        }
 });
 
 server.listen(PORT, () => {
