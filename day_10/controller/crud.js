3230b09dc713b8adf874b0061bbd3ef380cb603b
Ahmad Hammad Khan <ahmadhammadkhan21112005@gmail.com>
2026-04-08 15:00:37 +0530

    User data is being updated in MongoDB

--- day_10/controller/crud.js
+++ day_10/controller/crud.js
@@ -9,11 +9,30 @@ const createUser = async(user) => {
         console.log("Unable to create User" , error.message);
     }
 }
-createUser(
-    {
-        name : "Rohini",
-        email : "abcd@gmail.com",
-        password: "abc123",
-        gender: "F"
+const readUsers = async () => {
+    try {
+        const users = await User.find();
+        console.log(users);
+    } catch (error) {
+        console.log("Unable to read users" , error.message);
     }
-)
+}
+
+const updateUsers = async(email , userDetails) => {
+    try {
+        await User.updateOne({email : email},{$set : userDetails})
+        console.log("User has been updated successfully ");
+    } catch (error) {
+        console.log("Cannot update user " , error.message);
+    }
+}
+updateUsers("abcd@gmail.com" , {password: "newabc123"});
+// readUsers();
+// createUser(
+//     {
+//         name : "Hammad",
+//         email : "ahmadhammadkhan21112005@gmail.com",
+//         password: "ham123",
+//         gender: "Male"
+//     }
+// )
